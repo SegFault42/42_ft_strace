1204c50e24a2a640088bb9134105a7b5d457cc8d
SegFault42 <rabougue@student.42.fr>
2018-04-05 21:37:41 +0000

    PTRACE_TRACEME alternative

--- include/ft_strace.h
+++ include/ft_strace.h
@@ -1,4 +1,16 @@
 #ifndef FT_STRACE
 #define FT_STRACE
 
+#include <sys/user.h>
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+void	usage(int argc);
+
 #endif
--- source/main.c
+++ source/main.c
@@ -1,8 +1,46 @@
-#include "ft_strace.h"
+#include "../include/ft_strace.h"
+
+void	usage(int argc)
+{
+	if (argc == 1) {
+		fprintf(stderr,
+		"ft_strace: must have PROG [ARGS]\nTry 'strace -h' for more information.\n");
+		exit (EXIT_FAILURE);
+	}
+}
 
 int	main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
+	pid_t					child = 0;
+	char					*const args[] = {NULL};
+	int						status = 0;
+	struct user_regs_struct	regs;
+	uint64_t	old = 0;
+
+	usage(argc);
+	child = fork();
+	if (child == -1) {
+		perror("fork()");
+	}
+	else if (child == 0) {
+		execvp(argv[1], args);
+	} else {
+		kill(child, SIGSTOP);
+		ptrace(PTRACE_SEIZE, child, NULL, NULL);
+		waitpid(child, &status, 0);
+		while (1) {
+			ptrace(PTRACE_SYSCALL, child, NULL, NULL);
+			waitpid(child, &status, 0);
+			ptrace(PTRACE_GETREGS, child, NULL, &regs);
+			if (old != regs.orig_rax) {
+				printf("orig_rax : 0x%llx\n", regs.orig_rax);
+				old = regs.orig_rax;
+			}
+			ptrace(PTRACE_SYSCALL, child, NULL, NULL);
+			waitpid(child, &status, 0);
+			if (WIFEXITED(status))
+				break ;
+		}
+	}
 	return (0);
 }
