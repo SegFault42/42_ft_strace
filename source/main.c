ee6456f2ea3445838f2f5d9b8456c436bd7047e2
SegFault42 <rabougue@student.42.fr>
2018-04-06 11:53:34 +0000

    Getting all syscall ok

--- source/main.c
+++ source/main.c
@@ -15,7 +15,7 @@ int	main(int argc, char **argv)
 	char					*const args[] = {NULL};
 	int						status = 0;
 	struct user_regs_struct	regs;
-	uint64_t	old = 0;
+	uint64_t				old = 0;
 
 	usage(argc);
 	child = fork();
@@ -32,16 +32,14 @@ int	main(int argc, char **argv)
 			ptrace(PTRACE_SYSCALL, child, NULL, NULL);
 			waitpid(child, &status, 0);
 			ptrace(PTRACE_GETREGS, child, NULL, &regs);
-			if (old != regs.orig_rax) {
-				printf("orig_rax : 0x%llx\t | ", regs.orig_rax);
-				if (regs.rdi != 0x0) {
-					printf("rdi : %llx\t |", regs.rdi);
-				} else {
-					printf("NULL |");
-				}
-				old = regs.orig_rax;
-				puts("");
+			printf("orig_rax : 0x%llx\t | ", regs.orig_rax);
+			if (regs.rdi != 0x0) {
+				printf("rdi : %llx\t |", regs.rdi);
+			} else {
+				printf("NULL |");
 			}
+			old = regs.orig_rax;
+			puts("");
 			ptrace(PTRACE_SYSCALL, child, NULL, NULL);
 			waitpid(child, &status, 0);
 			if (WIFEXITED(status))
