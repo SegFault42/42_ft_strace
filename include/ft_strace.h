764a246f4d27b378a72eda93fbbbae70a520fcef
SegFault42 <rabougue@student.42.fr>
2018-04-07 18:49:00 +0000

    getting arg

--- include/ft_strace.h
+++ include/ft_strace.h
@@ -10,7 +10,36 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <sys/reg.h>
+
+#define BUFF_MAX	4096
+
+enum
+{
+	NONE,
+	INT,
+	U_INT,
+	LONG,
+	SIZE_T,
+	CHAR_PTR
+}			e_syscall;
+
+typedef struct		s_syscall
+{
+	char			*name; // Name of syscall
+	uint16_t		rax; // Syscall number
+	uint8_t			rdi;
+	uint8_t			rsi;
+	uint8_t			rdx;
+	uint8_t			r10;
+	uint8_t			r8;
+	uint8_t			r9;
+	uint8_t			pad[1];
+}					t_syscall;
 
 void	usage(int argc);
+
+
 
 #endif
--- source/main.c
+++ source/main.c
@@ -1,5 +1,11 @@
 #include "../include/ft_strace.h"
 
+const t_syscall	g_syscall_table[2] =
+{
+	{"read", 0, U_INT, CHAR_PTR, SIZE_T, NONE, NONE, NONE, {NONE}},
+	{NULL, NONE, NONE, NONE, NONE, NONE, NONE, NONE, {NONE}}
+};
+
 void	usage(int argc)
 {
 	if (argc == 1) {
@@ -16,6 +22,8 @@ int	main(int argc, char **argv)
 	int						status = 0;
 	struct user_regs_struct	regs;
 	uint64_t				old = 0;
+	char message[1000];
+	char* temp_char2 = message;
 
 	usage(argc);
 	child = fork();
@@ -32,11 +40,28 @@ int	main(int argc, char **argv)
 			ptrace(PTRACE_SYSCALL, child, NULL, NULL);
 			waitpid(child, &status, 0);
 			ptrace(PTRACE_GETREGS, child, NULL, &regs);
-			printf("orig_rax : 0x%llx\t | ", regs.orig_rax);
-			if (regs.rdi != 0x0) {
-				printf("rdi : %llx\t |", regs.rdi);
-			} else {
-				printf("NULL |");
+			/*printf("orig_rax : 0x%llx\t | ", regs.orig_rax);*/
+			/*if (regs.rdi != 0x0) {*/
+				/*printf("rdi : %llx\t |", regs.rdi);*/
+			/*} else {*/
+				/*printf("NULL |");*/
+			/*}*/
+
+			/*if (regs.orig_rax == 0x0) {*/
+				/*if ()*/
+				/*[>sprintf(fdpath,"/proc/%u/fd/%llu", child, regs.rdi);<]*/
+				/*[>size = readlink(fdpath, &filepath, 256);<]*/
+				/*[>filepath[size] = '\0';<]*/
+				/*[>printf("File-%s-\n", filepath);<]*/
+			/*}*/
+			if (regs.orig_rax == 0x0) {
+				printf("Syscall = %s\t| ", g_syscall_table[regs.orig_rax].name);
+				if (g_syscall_table[regs.orig_rax].rdi == U_INT) {
+					printf("rdi = %lld\t| ", regs.rdi);
+				}
+				if (g_syscall_table[regs.orig_rax].rsi == CHAR_PTR) {
+					printf("rsi = %llu", regs.rsi);
+				}
 			}
 			old = regs.orig_rax;
 			puts("");
