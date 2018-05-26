bd280f4699a7c005bb38733aa2d1b05d799fc315
SegFault42 <SegFault42@protonmail.com>
2018-05-26 18:54:02 +0200

    segfault fixed

--- source/main.c
+++ source/main.c
@@ -23,17 +23,24 @@ int	main(int argc, char **argv, char **env)
 
 	usage(argc);
 
-	if (argv[1][0] != '/')
+	// If we don't have absolute path, we need to search the binary path
+	if (argv[1][0] != '/') {
 		path = get_path_bin(argv[1]);
-	else
-		path = argv[1];
+		if (!path) {
+			dprintf(2, "ft_strace: Can't stat '%s': No such file or directory\n", argv[1]);
+			return (EXIT_FAILURE);
+		}
+	}
 
 	child = fork();
 	if (child == -1) {
 		perror("fork()");
 	}
 	else if (child == 0) {
-		execve(path, &argv[1], env);
+		if (path == NULL)
+			execve(argv[1], &argv[1], env);
+		else
+			execve(path, &argv[1], env);
 	} else {
 		kill(child, SIGSTOP);
 		ptrace(PTRACE_SEIZE, child, NULL, NULL);
@@ -45,8 +52,6 @@ int	main(int argc, char **argv, char **env)
 
 			print(&regs, loop);
 
-			/*ptrace(PTRACE_SYSCALL, child, NULL, NULL);*/
-			/*waitpid(child, &status, 0);*/
 			if (WIFEXITED(status))
 				break ;
 			++loop;
